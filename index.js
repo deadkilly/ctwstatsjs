4b6086ab31e5aeac0088b3fae5cf4ceeb244da02
deadkilly <69108530+deadkilly@users.noreply.github.com>
2021-12-05 11:00:41 +0200

    Update index.js
    Added achievements lololololololol

--- index.js
+++ index.js
@@ -42,11 +42,26 @@ client.on('message', async message => {
                     let content = JSON.parse(response.body);
                     let kills = content.player.achievements.arcade_ctw_slayer;
                     let caps = content.player.achievements.arcade_ctw_oh_sheep;
+		    let achievements_ids = content.player.achievementsOneTime;
+                    let achievementsar = [];
+                    if (achievements_ids.includes("arcade_ctw_no_need")) achievementsar.push("No need");
+                    if (achievements_ids.includes("arcade_ctw_hey_there")) achievementsar.push("Hey there");
+                    if (achievements_ids.includes("arcade_ctw_first")) achievementsar.push("First");
+                    if (achievements_ids.includes("arcade_ctw_comeback")) achievementsar.push("Comeback");
+                    if (achievements_ids.includes("arcade_ctw_ninja")) achievementsar.push("Ninja");
+                    if (achievements_ids.includes("arcade_ctw_fashionably_late")) achievementsar.push("Fashionably late");
+                    if (achievements_ids.includes("arcade_ctw_safety_is_an_illusion")) achievementsar.push("Safety is an illusion");
+                    if (achievements_ids.includes("arcade_ctw_mvp")) achievementsar.push("MVP");
+                    if (achievements_ids.includes("arcade_ctw_i_can_be_anything")) achievementsar.push("I can be anything");
+                    if (achievements_ids.includes("arcade_ctw_magician")) achievementsar.push("Magician");
+                    if (achievements_ids.includes("arcade_ctw_right_place_right_time")) achievementsar.push("Right place, right time");
+                    let achivement_pos = 11;
+		
                     message.channel.send(
                         new Discord.MessageEmbed()
                             .setTitle(`${nickname}'s Stats`)
                             .setColor(roleColor === "#000000" ? "#ffffff" : roleColor)
-                            .setDescription(`Captures: **${caps}**\nKills/Assists: **${kills}**`)
+                            .setDescription(`Captures: **${caps}**\nKills/Assists: **${kills}**\nAchivements: **${achievementsar.length}/${achivement_pos}**`)
                             .setThumbnail(`https://visage.surgeplay.com/bust/512/${uuid.toString()}`)
                     );
                 })
